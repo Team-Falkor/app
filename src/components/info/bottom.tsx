3721cfb00e151904e75b6c3a0fcca8e8f70d3d55
prostarz <tommyspam2000@gmail.com>
2024-09-12 05:32:29 +0100

    (fix): requirements from steam

--- src/components/info/bottom.tsx
+++ src/components/info/bottom.tsx
@@ -3,21 +3,9 @@ import GameMedia from "@/components/info/media";
 import SimilarGames from "@/components/info/similar";
 import PcSpecs from "@/components/info/specs";
 import { InfoReturn } from "@/lib/api/igdb/types";
-import { useOS } from "@/stores/settings";
-import { useMemo } from "react";
 
 const InfoBottom = (props: InfoReturn & InfoProps) => {
-  const { platform } = useOS();
   const { similar_games } = props;
-
-  const findRequirements = useMemo(() => {
-    if (!props.steam?.data) return;
-    if (platform === "unknown") return;
-
-    // const data = props.steam?.data;
-
-    return props.steam?.data.pc_requirements;
-  }, [platform]);
 
   return (
     <div className="flex flex-col gap-6 mt-5">
@@ -25,7 +13,7 @@ const InfoBottom = (props: InfoReturn & InfoProps) => {
 
       <GameMedia {...props} />
 
-      <PcSpecs {...findRequirements} />
+      <PcSpecs {...props.steam?.data.pc_requirements} />
     </div>
   );
 };
