1d1afb58ae89c8414c697cdb7f48978214e6bc2a
prostarz <tommyspam2000@gmail.com>
2024-09-21 20:14:17 +0100

    improve HLTB api wrapper

--- src/lib/api/hltb/index.ts
+++ src/lib/api/hltb/index.ts
@@ -1,46 +1,68 @@
 import { formatName } from "@/lib/helpers/formatName";
-import { fetch } from "@tauri-apps/plugin-http";
 import { load } from "cheerio";
 import { BaseApi } from "../base";
 
-export class HLTB extends BaseApi {
-  public static BASE_URL: string = "https://howlongtobeat.com/";
-  public static DETAIL_URL: string = `${this.BASE_URL}game?id=`;
-  public static SEARCH_URL: string = `${this.BASE_URL}api/search/`;
-  public static IMAGE_URL: string = `${this.BASE_URL}games/`;
+let instance: HLTB | null = null;
 
-  private searchKey: string | undefined;
+export class HLTB extends BaseApi {
+  private searchKey: string | null = null;
+
+  public static BASE_URL: string = "https://howlongtobeat.com/";
+  public static DETAIL_URL: string = `${HLTB.BASE_URL}game?id=`;
+  public static SEARCH_URL: string = `${HLTB.BASE_URL}api/search/`;
+  public static IMAGE_URL: string = `${HLTB.BASE_URL}games/`;
+
+  constructor() {
+    super();
+    instance ||= this;
+    return instance;
+  }
 
   private static readonly SEARCH_KEY_PATTERN =
     /"\/api\/search\/".concat\("([a-zA-Z0-9]+)"\)/g;
 
-  async search(query: string) {
-    if (!this.searchKey) this.searchKey = await this.getSearchKey();
+  async search(query: string, retry = true): Promise<HLTBSearchGameData> {
+    this.searchKey ||= await this.getSearchKey();
+    const searchUrlWithKey = HLTB.SEARCH_URL + this.searchKey;
 
-    const res = await fetch(`${HLTB.SEARCH_URL}${this.searchKey}`, {
-      method: "POST",
-      body: JSON.stringify({
-        searchType: "games",
-        searchTerms: formatName(query).split(" "),
-        searchPage: 1,
-        size: 100,
-      }),
-      headers: {
-        "User-Agent":
-          "Chrome: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-
-        "content-type": "application/json",
-        referer: "https://howlongtobeat.com/",
-      },
+    const body = JSON.stringify({
+      searchType: "games",
+      searchTerms: formatName(query).split(" "),
+      searchPage: 1,
+      size: 100,
     });
 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch: ${res.status}`);
+    try {
+      const res = await this.makeReq(searchUrlWithKey, {
+        method: "POST",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 6.0; XT1069 Build/MPB24.65-34-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.105 Mobile Safari/537.36",
+          origin: "https://howlongtobeat.com",
+          referer: "https://howlongtobeat.com",
+        },
+        body: body,
+      });
+
+      if (res.status === 404 && retry) {
+        this.searchKey = null;
+        return this.search(query, false);
+      }
+
+      if (res.status !== 200) {
+        throw new Error(`Got non-200 status code from howlongtobeat.com [${
+          res.status
+        }]
+          ${res.statusText}
+        `);
+      }
+
+      const data: HLTBSearchGameData = await res.json();
+
+      return data;
+    } catch (error) {
+      throw new Error((error as Error).message);
     }
-
-    const data: HLTBSearchGameData = await res.json();
-
-    return data;
   }
 
   private async getSearchKey(
