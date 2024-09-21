0ce2f5914a3b79f3a2442992775fa38da720e8b9
prostarz <tommyspam2000@gmail.com>
2024-09-21 20:57:24 +0100

    lib.old.rs

---
+++ src-tauri/src/lib.old.rs
@@ -0,0 +1,14 @@
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_http::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
