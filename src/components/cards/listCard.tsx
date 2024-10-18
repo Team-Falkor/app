58887b38a4c2a07ec58c7050eb1c6086aa6bfbfb
prostarz <tommyspam2000@gmail.com>
2024-10-18 22:41:30 +0100

    fix: broken size on list cards, and add text balance to title

--- src/components/cards/listCard.tsx
+++ src/components/cards/listCard.tsx
@@ -1,7 +1,6 @@
 import { ListGame } from "@/@types";
 import { Link } from "@tanstack/react-router";
 import IGDBImage from "../IGDBImage";
-import { buttonVariants } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
 
 type ListCardProps = ListGame;
@@ -10,14 +9,7 @@ const ListCard = ({ game_id, title, image }: ListCardProps) => {
   return (
     <Card className="group relative m-0 mt-3 w-[200px] rounded-t-lg p-0 overflow-hidden">
       <CardContent className="p-0 m-0">
-        <Link
-          className={buttonVariants({
-            variant: "secondary",
-            className: "mt-5",
-          })}
-          to={`/info/$id`}
-          params={{ id: game_id.toString() }}
-        >
+        <Link to={`/info/$id`} params={{ id: game_id.toString() }}>
           <div className="relative overflow-hidden rounded-t-lg 2 group focus:outline-none dark:ring-offset-gray-900">
             <IGDBImage
               imageId={
@@ -35,7 +27,7 @@ const ListCard = ({ game_id, title, image }: ListCardProps) => {
           <div className="absolute inset-0 z-20 flex flex-col items-center justify-center transition duration-300 ease-out translate-y-full rounded opacity-0 cursor-pointer bg-slate-700 bg-opacity-80 group-focus-within:translate-y-0 group-focus-within:opacity-100 group-hover:translate-y-0 group-hover:opacity-100 group-focus:translate-y-0 group-focus:opacity-100">
             <div className="flex flex-col items-center justify-center w-full gap-4 p-2">
               <div className="flex flex-col w-full gap-1">
-                <h4 className="font-medium text-center text-white whitespace-pre-line break-before-avoid">
+                <h4 className="font-medium text-center text-white whitespace-pre-line break-before-avoid text-balance">
                   {title}
                 </h4>
               </div>
