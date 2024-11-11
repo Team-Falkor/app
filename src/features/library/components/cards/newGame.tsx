e9d925bf9636d4eaac997c144f8fa092af63445d
prostarz <tommyspam2000@gmail.com>
2024-11-11 18:17:43 +0000

    fix(ui): tooltip going under new game card

--- src/features/library/components/cards/newGame.tsx
+++ src/features/library/components/cards/newGame.tsx
@@ -2,10 +2,10 @@ import { PlusIcon } from "lucide-react";
 
 const NewGameCard = () => {
   return (
-    <div className="h-44 w-full rounded-t-lg p-0 overflow-hidden relative group cursor-pointer">
+    <div className="relative w-full p-0 overflow-hidden rounded-t-lg cursor-pointer h-44 group">
       {/* BG */}
       <div className="w-full h-full rounded-md bg-gradient-to-tr from-blue-400 to-purple-400" />
-      <div className="absolute inset-0 z-20 flex items-center justify-center transition-all opacity-100 bg-gradient-to-tl from-background to-transparent hover:opacity-85">
+      <div className="absolute inset-0 z-0 flex items-center justify-center transition-all opacity-100 bg-gradient-to-tl from-background to-transparent hover:opacity-85">
         <PlusIcon fill="white" className="size-10" />
       </div>
     </div>
