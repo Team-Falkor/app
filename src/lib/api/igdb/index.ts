286d38e910a8cd17a5f0b0fcf2b982eb81f7f0a3
prostarz <tommyspam2000@gmail.com>
2024-11-13 16:01:54 +0000

    fix(search): not returning certian games from igdb

--- src/lib/api/igdb/index.ts
+++ src/lib/api/igdb/index.ts
@@ -98,7 +98,7 @@ class IGDB extends BaseApi {
 
     const data = await this.request<IGDBReturnDataType[]>("games", {
       search: realQuery,
-      where: `platforms.abbreviation = "PC" & category = 0 & version_parent = null`,
+      where: `platforms.abbreviation = "PC" & version_parent = null`,
     });
 
     return data;
