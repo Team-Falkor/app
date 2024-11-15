2297a69ecc4ca0d74806cb2e4f225819b3e27140
prostarz <tommyspam2000@gmail.com>
2024-11-15 13:25:48 +0000

    fix(trailer): add back translation

--- src/components/trailer/index.tsx
+++ src/components/trailer/index.tsx
@@ -2,14 +2,19 @@ import { IGDBReturnDataType } from "@/lib/api/igdb/types";
 import { Button } from "../ui/button";
 import { Dialog, DialogTrigger } from "../ui/dialog";
 import TrailerDialogContent from "./dialogContent";
+import { useLanguageContext } from "@/contexts/I18N";
 
 type Props = Pick<IGDBReturnDataType, "name" | "videos">;
 
 const TrailerButton = (props: Props) => {
+  const { t } = useLanguageContext();
+
   return (
     <Dialog>
       <DialogTrigger>
-        <Button variant="secondary">Trailer</Button>
+        <Button variant="secondary" className="capitalize">
+          {t("trailer")}
+        </Button>
       </DialogTrigger>
 
       <TrailerDialogContent {...props} />
