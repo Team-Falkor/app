f375accbf27c751b930053d06eb9d16efbe26cd9
prostarz <tommyspam2000@gmail.com>
2024-12-14 16:10:32 +0000

    fix: protonDb badge

--- src/hooks/useProtonDb.ts
+++ src/hooks/useProtonDb.ts
@@ -1,12 +1,22 @@
 import { ProtonDBSummary } from "@/@types";
+import { invoke } from "@/lib";
 import { useQuery } from "@tanstack/react-query";
+
+interface Response {
+  data: ProtonDBSummary;
+  success: boolean;
+  error?: string;
+}
 
 export const useProtonDb = (appId: string) => {
   const getProtonDb = async (): Promise<ProtonDBSummary | null> => {
-    const response = await window.ipcRenderer.invoke(
+    const response = await invoke<Response>(
       "request",
       `https://www.protondb.com/api/v1/reports/summaries/${appId}.json`
     );
+
+    if (!response?.success) return null;
+
     return response?.data;
   };
 
